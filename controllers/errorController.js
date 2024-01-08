692763216be0bce2cab3ff0855bb93fcee3d867d
kgbinh <binhkhongg@gmail.com>
2024-01-08 14:32:58 +0700

    điều chỉnh xuất lỗi cho môi trường production và môi trường development

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,46 @@
+const CustomError = require('../ultils/CustomError');
+
+const handleCastErrorDB = (error) => {
+  const message = `Invalid ${path}:${value}`;
+  return new CustomError(message, 404);
+};
+const sendErrorDev = (res, error) => {
+  res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+    isOperational: error.isOperational,
+    error: error,
+    stack: error.stack,
+  });
+};
+const sendErrorPro = (res, error) => {
+  if (error.isOperational) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+    ////Nếu là lỗi developerment thì giấu đi
+  } else {
+    //1.LOG LỖI RA
+    console.log(error);
+    //2.CHUYỂN SANG MỘT MÃ LỖI KHÁC
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!!',
+    });
+  }
+};
 module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error';
 
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(res, error);
+  } else if (process.env.NODE_ENV === 'production') {
+    let err = { ...error };
+    if ((error.name = 'CastError')) {
+      err = handleCastErrorDB(err);
+    }
+    sendErrorPro(res, err);
+  }
 };
