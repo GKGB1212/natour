2cb056e0dc00ee0bc8cdd7a6dc93ce9cf8f52035
kgbinh <binhkhongg@gmail.com>
2024-01-10 21:20:18 +0700

    Thêm thư viện validator, thêm userSchema và userModel

---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please tell us your email!'],
+    unique: true,
+    lowcase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minLength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please cofirm your password'],
+  },
+});
+const User = mongoose.model('User', userSchema);
