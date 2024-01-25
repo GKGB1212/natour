205cea564254f53612e99d32d137e1e49b3b23ab
kgbinh <binhkhongg@gmail.com>
2024-01-25 15:43:54 +0700

    Thêm thư viện helmet tăng bảo mật cho http header

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const CustomError = require('./ultils/CustomError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,19 +10,23 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+//set security HTTP header
+app.use(helmet());
+
 //thêm middleware để parse body của request
-app.use(express.json());
+app.use(express.json({ limit: '10KB' }));
+
 //thêm middleware để log
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//Limit requests from same API
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 phút
   max: 2, // Số lượng yêu cầu tối đa trong khoảng thời gian trên
   message: 'Too many request from this IP, please try again in 15 minutes!',
 });
-
 app.use('/api', limiter);
 
 app.use((req, res, next) => {
@@ -46,14 +51,3 @@ app.all('*', (req, res, next) => {
 app.use(globalErrorHandler);
 
 module.exports = app;
-
-/*
-//get all tours
-app.get('/api/v1/tours', getAllTours);
-//get tour by id
-app.get('/api/v1/tours/:id', getTour);
-//add new tour
-app.post('/api/v1/tours', addNewTour);
-//update tour
-app.patch('/api/v1/tours/:id', updateTour);
-*/
